dcbc8feafe68418237f512607e8fe5d0ec635432
carlos orrego <carlosorrego5151@gmail.com>
2025-02-15 23:21:22 -0500

    feat: add concepts typescript

--- 2.typescript/src/main.ts
+++ 2.typescript/src/main.ts
@@ -1,4 +1,10 @@
-import './style.css'
+// import './style.css'
+// import './topics/01-basic-types'
+// import "./topics/02-object-interfaces";
+// import "./topics/03-functions";
+// import "./topics/04-functions-with-objects";
+// import "./topics/05-class-creation";
+import "./topics/06-class-dependency-injection";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
