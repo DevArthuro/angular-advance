fa33efef291e5b78d9a52f32365acb57339a3dd9
carlos orrego <carlosorrego5151@gmail.com>
2025-02-24 23:59:19 -0500

    feat: add router handler

--- 4.counter-with-signals/src/app/app.component.ts
+++ 4.counter-with-signals/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from "./pages/counter/counter.component";
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
