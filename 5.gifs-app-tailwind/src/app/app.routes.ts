763b6349b9e12a2f9b762e5f6731c11d00217a9b
carlos orrego <carlosorrego5151@gmail.com>
2025-02-27 23:01:43 -0500

    feat: set up initial project

---
+++ 5.gifs-app-tailwind/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page.component'), // The import component has default
+    // child routes
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/tranding-page/tranding-page.component'),
+      },
+      {
+        path: 'searching',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component'),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
---
+++ 5.gifs-app-tailwind/src/app/gifs/pages/tranding-page/tranding-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tranding-page',
+  imports: [],
+  templateUrl: './tranding-page.component.html'
+})
+export default class TrandingPageComponent {
+
+}
