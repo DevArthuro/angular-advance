a69e428b49ff07862505636676dca4a9ec8b75d6
carlos orrego <carlosorrego5151@gmail.com>
2025-03-01 22:20:49 -0500

    feat: add types giphy api

--- 5.gifs-app-tailwind/src/environments/environment.development.ts
+++ 5.gifs-app-tailwind/src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
-    NAME_APP: "Dev",
-    VERSION: 1.0,
-    SLOGAN: "dev test for slogan",
+  NAME_APP: 'Dev',
+  VERSION: 1.0,
+  SLOGAN: 'dev test for slogan',
+
+  giphyApiKey: '02BZgcipRdcIeOjZQhurwEJUVNxwSDpLX',
 };
--- 5.gifs-app-tailwind/src/environments/environment.ts
+++ 5.gifs-app-tailwind/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   NAME_APP: 'Prod',
   VERSION: 1.0,
-  SLOGAN: "Manage your actions and activities",
+  SLOGAN: 'Manage your actions and activities',
+
+  giphyApiKey: '02BZgcipRdcIeOjZQhurwEJUVNxwSDpLX',
 };
