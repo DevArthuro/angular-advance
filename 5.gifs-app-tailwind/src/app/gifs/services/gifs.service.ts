24f2d24a44286ccd88853b349f049a8ecc14bbd3
carlos orrego <carlosorrego5151@gmail.com>
2025-03-02 20:54:57 -0500

    feat: add query search and consume

--- 5.gifs-app-tailwind/src/app/gifs/services/gifs.service.ts
+++ 5.gifs-app-tailwind/src/app/gifs/services/gifs.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject, signal } from '@angular/core';
 import { GIF, GiphyResponse } from '../interfaces/giphy.interface';
 import { environment } from 'src/environments/environment';
 import { GiphyMapper } from '../mapper/giphyMapper';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -23,13 +24,30 @@ export class GifService {
           api_key: environment.giphyApiKey,
           limit: 30,
           offset: 0,
-          rating: 0,
         },
       })
       .subscribe((res) => {
         const data = GiphyMapper.parseDataGiphyToGiphySchema(res.data);
-        console.log(data)
         this.gifs.set(data);
+        this.load.set(false);
       });
+  }
+
+  searchGifs(querySearch: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.baseUrlGiphy}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 30,
+          offset: 0,
+          q: querySearch,
+        },
+      })
+      .pipe(
+        map(({ data }) => GiphyMapper.parseDataGiphyToGiphySchema(data)),
+        tap((data) => {
+          console.log(data);
+        })
+      );
   }
 }
