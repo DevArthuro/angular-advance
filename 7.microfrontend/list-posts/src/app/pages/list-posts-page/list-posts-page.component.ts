ef2b0485fb8f04302eebb6b5b858a931292f1219
carlos orrego <carlosorrego5151@gmail.com>
2025-03-05 12:18:30 -0500

    feat: add microfronts remotes native federation

---
+++ 7.microfrontend/list-posts/src/app/pages/list-posts-page/list-posts-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'list-posts-page',
+  imports: [],
+  templateUrl: './list-posts-page.component.html',
+  styleUrl: './list-posts-page.component.scss'
+})
+export default class ListPostsPageComponent {
+
+}
---
+++ 7.microfrontend/register-post/src/app/pages/register-form-page/register-form-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register-form-page',
+  imports: [],
+  templateUrl: './register-form-page.component.html',
+  styleUrl: './register-form-page.component.scss'
+})
+export default class RegisterFormPageComponent {
+
+}
