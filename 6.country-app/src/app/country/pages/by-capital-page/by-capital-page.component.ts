ceaa790e31def1201feaf849deb8a3b35382a3db
carlos orrego <carlosorrego5151@gmail.com>
2025-03-06 23:43:30 -0500

    refactor: test signal computed resources for request

--- 6.country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 6.country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, ResourceStatus, signal } from '@angular/core';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { TableComponent } from '../../components/table/tableList.component';
 import { CountryService } from '../../services/contriesRest.service';
-import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'country-by-capital-page',
   imports: [SearchInputComponent, TableComponent],
@@ -10,26 +10,13 @@ import { Country } from '../../interfaces/country.interface';
 })
 export default class ByCapitalPageComponent {
   countryService = inject(CountryService);
-  countries = signal<Country[]>([]);
-  isLoading = signal<boolean>(false);
-  isError = signal<string | null>(null);
+  query = signal<string>('');
 
-  onSearch(value: string) {
-    if (this.isLoading()) return;
-
-    this.isLoading.update((prev) => !prev);
-    this.countryService.getCapitalByQuery(value).subscribe({
-      next: (response) => {
-        this.countries.set(response);
-        console.log(response)
-      },
-      error: (error) => {
-        this.isError.set('Has ocurr the error');
-        this.isLoading.set(false);
-      },
-      complete: () => {
-        this.isLoading.set(false);
-      },
-    });
-  }
+  callServiceCountries = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request: { query } }) => {
+      if (!query) return [];
+      return await firstValueFrom(this.countryService.getCapitalByQuery(query));
+    },
+  });
 }
--- 6.country-app/src/app/country/services/contriesRest.service.ts
+++ 6.country-app/src/app/country/services/contriesRest.service.ts
@@ -1,23 +1,29 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { ResponseCountry } from "../interfaces/res.interface";
-import { map } from "rxjs";
-import { CountryMapper } from "../mapper/country.mapper";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ResponseCountry } from '../interfaces/res.interface';
+import { catchError, map, throwError } from 'rxjs';
+import { CountryMapper } from '../mapper/country.mapper';
 
 const BASE_URL_API = 'https://restcountries.com/v3.1';
 
 @Injectable({
-    providedIn: "root"
+  providedIn: 'root',
 })
 export class CountryService {
+  http = inject(HttpClient);
 
-    http = inject(HttpClient)
+  getCapitalByQuery(query: string) {
+    query = query.toLowerCase();
 
-    getCapitalByQuery(query: string) {
-        query = query.toLowerCase();
-
-        return this.http
-          .get<ResponseCountry[]>(`${BASE_URL_API}/capital/${query}`)
-          .pipe(map((value) => CountryMapper.parseResponseListToCountryList(value)));
-    }
+    return this.http
+      .get<ResponseCountry[]>(`${BASE_URL_API}/capital/${query}`)
+      .pipe(
+        map((value) => CountryMapper.parseResponseListToCountryList(value)),
+        catchError((error) => {
+          return throwError(
+            () => new Error('Has ocurrer error to get capitals...')
+          );
+        })
+      );
+  }
 }
