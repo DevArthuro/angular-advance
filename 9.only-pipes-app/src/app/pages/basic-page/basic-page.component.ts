9eebc426ad92ae056bf5aa69eed77be1eecff6d7
carlos orrego <carlosorrego5151@gmail.com>
2025-03-23 15:35:01 -0500

    feat: add basic pipes - date pipes - number pipes

---
+++ 9.only-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,27 @@
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
+  templateUrl: './basic-page.component.html',
+})
+export default class BasicPageComponent {
+  nameLower = signal('carlos');
+  nameUpper = signal('CARLOS');
+  fullName = signal('cArLoS oRREgo');
+
+  customDate = signal(new Date())
+
+
+  tickingDateEffect = effect((onCleanUp) => {
+      const interval = setInterval(() => {
+        this.customDate.set(new Date())
+        console.log("tick")
+      }, 1000)
+
+      onCleanUp(() => {
+        clearInterval(interval)
+      })
+  })
+}
