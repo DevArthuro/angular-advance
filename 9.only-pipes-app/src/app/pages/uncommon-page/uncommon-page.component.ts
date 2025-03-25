f76a1659f2d7d574f9b37836fa9dfabd2c42ba8f
carlos orrego <carlosorrego5151@gmail.com>
2025-03-24 23:52:54 -0500

    feat: use ng child and i18nselect

--- 9.only-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 9.only-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const cliente1 = {
+  name: 'Fernando',
+  gender: 'male',
+  age: 39,
+  address: 'Ottawa, Canadá',
+};
+
+const cliente2 = {
+  name: 'Melissa',
+  gender: 'female',
+  age: 39,
+  address: 'Toronto, Canadá',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
-  templateUrl: './uncommon-page.component.html'
+  imports: [CardComponent, I18nSelectPipe],
+  templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
+  client = signal(cliente1);
 
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    if (this.client() === cliente1) {
+      this.client.set(cliente2);
+      return;
+    }
+    this.client.set(cliente1);
+  }
 }
