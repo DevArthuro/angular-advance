f8c255940c33c9e0c514f55039c97db3b755a2a8
carlos orrego <carlosorrego5151@gmail.com>
2025-03-25 22:46:09 -0500

    feat: example use i18pluralpipe

--- 9.only-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 9.only-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, TitleCasePipe } from '@angular/common';
 
 const cliente1 = {
   name: 'Fernando',
@@ -18,7 +18,7 @@ const cliente2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, TitleCasePipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -36,4 +36,31 @@ export default class UncommonPageComponent {
     }
     this.client.set(cliente1);
   }
+
+  clients = signal([
+    'Carlos',
+    'Juan',
+    'Pablo',
+    'Pedro',
+    'persona1',
+    'persona2',
+    'persona3',
+    'persona4',
+    'persona5',
+  ]);
+
+  deleteClient() {
+    this.clients.update((clients) => {
+      const newArray = [...clients];
+      newArray.pop();
+      return newArray;
+    });
+  }
+
+  clientsMap = signal({
+    '=0': 'no tenemos clientes',
+    '=1': 'tenemos un cliente',
+    '=2': 'tenemos dos clientes',
+    other: 'tenemos # clientes'
+  })
 }
