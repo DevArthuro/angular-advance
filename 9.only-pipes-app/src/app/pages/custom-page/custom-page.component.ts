11bab29aded32131a78896082fd66c363a14dfeb
carlos orrego <carlosorrego5151@gmail.com>
2025-03-26 23:56:37 -0500

    feat: add pipe boolean to text

--- 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { TooglePipe } from '../../pipes/pipes.pipe';
 import { heroes } from '../../data/heroes.data';
+import { BooleanToTextPipe } from '../../pipes/booleanToTextPipe.pipe';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [TooglePipe],
+  imports: [TooglePipe, BooleanToTextPipe, TitleCasePipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
