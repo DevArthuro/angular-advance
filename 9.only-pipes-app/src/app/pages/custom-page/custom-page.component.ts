dbfc7c23538d4f0e37b52b06fe0eefc013bcd9f4
carlos orrego <carlosorrego5151@gmail.com>
2025-03-27 00:33:13 -0500

    feat: add convert text

--- 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -2,11 +2,20 @@ import { Component, signal } from '@angular/core';
 import { TooglePipe } from '../../pipes/pipes.pipe';
 import { heroes } from '../../data/heroes.data';
 import { BooleanToTextPipe } from '../../pipes/booleanToTextPipe.pipe';
-import { TitleCasePipe } from '@angular/common';
+import { LowerCasePipe, TitleCasePipe } from '@angular/common';
+import { ColorConvertPipe } from '../../pipes/colortConvert.pipe';
+import { CreatorCasePipe } from '../../pipes/creatorPipe.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [TooglePipe, BooleanToTextPipe, TitleCasePipe],
+  imports: [
+    TooglePipe,
+    BooleanToTextPipe,
+    TitleCasePipe,
+    ColorConvertPipe,
+    CreatorCasePipe,
+    LowerCasePipe,
+  ],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
