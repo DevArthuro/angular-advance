d0338135162a62102993f3d395492cec07ccb85a
carlos orrego <carlosorrego5151@gmail.com>
2025-03-27 01:05:02 -0500

    feat: pipes filter and search elements in loop

--- 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -5,6 +5,9 @@ import { BooleanToTextPipe } from '../../pipes/booleanToTextPipe.pipe';
 import { LowerCasePipe, TitleCasePipe } from '@angular/common';
 import { ColorConvertPipe } from '../../pipes/colortConvert.pipe';
 import { CreatorCasePipe } from '../../pipes/creatorPipe.pipe';
+import { HeroSortPipe } from '../../pipes/sort-by-attribute.pipe';
+import { Hero } from '../../interfaces/hero.interface';
+import { SearchHeroesPipe } from '../../pipes/search-heroes.pipe';
 
 @Component({
   selector: 'app-custom-page',
@@ -15,6 +18,8 @@ import { CreatorCasePipe } from '../../pipes/creatorPipe.pipe';
     ColorConvertPipe,
     CreatorCasePipe,
     LowerCasePipe,
+    HeroSortPipe,
+    SearchHeroesPipe,
   ],
   templateUrl: './custom-page.component.html',
 })
@@ -22,6 +27,9 @@ export default class CustomPageComponent {
   name = signal('carlos');
   convert = signal(false);
   heroes = signal(heroes);
+
+  keySort = signal<keyof Hero | null>(null);
+  search = signal('');
 
   changeState() {
     return this.convert.update((value) => !value);
