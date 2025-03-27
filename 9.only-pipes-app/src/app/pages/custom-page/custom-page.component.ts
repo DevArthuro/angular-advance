d51ead793ff5e3b50f5279b60ee5e1b0c45aa056
carlos orrego <carlosorrego5151@gmail.com>
2025-03-26 23:47:02 -0500

    feat: create custom pipe and create template exercise

--- 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 9.only-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TooglePipe } from '../../pipes/pipes.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
-  templateUrl: './custom-page.component.html'
+  imports: [TooglePipe],
+  templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
+  name = signal('carlos');
+  convert = signal(false);
+  heroes = signal(heroes);
 
+  changeState() {
+    return this.convert.update((value) => !value);
+  }
 }
