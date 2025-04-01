a4d893515bb281a448875a738d63432fac6944b0
carlos orrego <carlosorrego5151@gmail.com>
2025-03-31 23:37:13 -0500

    feat: add reactive forms module and interactive

--- 10.reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 10.reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,16 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from "@angular/forms"
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+  formProduct = new FormGroup({
+    name: new FormControl(""),
+    price: new FormControl(0),
+    isStorage: new FormControl(0)
+  });
+}
