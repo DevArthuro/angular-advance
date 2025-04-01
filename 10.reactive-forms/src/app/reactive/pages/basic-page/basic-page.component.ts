f5eb0631489d7da8168269eb6843da788fa3a502
carlos orrego <carlosorrego5151@gmail.com>
2025-04-01 00:15:27 -0500

    feat: add form builder and validators

--- 10.reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 10.reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from "@angular/forms"
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms"
 
 @Component({
   selector: 'app-basic-page',
@@ -8,9 +8,41 @@ import { FormGroup, FormControl, ReactiveFormsModule } from "@angular/forms"
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
-  formProduct = new FormGroup({
-    name: new FormControl(""),
-    price: new FormControl(0),
-    isStorage: new FormControl(0)
+  private fb = inject(FormBuilder);
+
+  // formProduct = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   isStorage: new FormControl(0),
+  // });
+
+  formProduct: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(1000)]],
+    isStorage: [0, [Validators.required, Validators.min(0)]],
   });
+
+
+  isErrorField(field: string) {
+    return !!this.formProduct.controls[field].errors
+  }
+
+  getMessageError(field: string) {
+    if (!this.formProduct.controls[field]) return null;
+
+    const errors = this.formProduct.controls[field].errors?? {}
+
+    for (const key in errors) {
+      switch(key) {
+        case 'required':
+          return 'El campo es requerido';
+        case 'minlength':
+          return `Mìnimo de ${errors['minlength'].requiredLength} caracteres`;
+        case 'min':
+          return `Valor mìnimo de ${errors["min"].min}`;
+      }
+    }
+
+    return null
+  }
 }
